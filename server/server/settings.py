4a304a725390b97c76c0e1010c090485d3b086e8
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-23 02:01:44 +0530

    Create settings.py

---
+++ server/server/settings.py
@@ -0,0 +1,25 @@
+SECRET_KEY = 'django-secret-key'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djangoapp',
+]
+
+ROOT_URLCONF = 'server.urls'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+STATIC_URL = '/static/'

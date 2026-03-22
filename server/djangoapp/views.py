2db02e346e22f24ae229c3dc91c740d7789340a4
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-23 01:54:38 +0530

    Create views.py

---
+++ server/djangoapp/views.py
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Contact Us</h1>
+    <p>Email: support@dealership.com</p>
+    <p>Phone: +91 9876543210</p>
+</body>
+</html>

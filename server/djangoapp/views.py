3df570b59c5efdef5976be52596e762d3b5b4689
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-24 22:18:08 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Dealer
+
 
 def home(request):
     return render(request, 'Home.html')
 
+
 def about(request):
     return render(request, 'About.html')
+
 
 def contact(request):
     return render(request, 'Contact.html')
 
 
-#  Q16 / Q17 MAIN FUNCTION
-def get_dealers(request):
-    dealers = [
-        {"name": "BMW Delhi", "city": "Delhi"},
-        {"name": "Audi Mumbai", "city": "Mumbai"},
-        {"name": "Tesla Bangalore", "city": "Bangalore"},
-    ]
+#  Q17 FINAL FUNCTION
+@login_required
+def get_dealers_loggedin(request):
+    dealers = Dealer.objects.all()
 
-    return render(request, 'Home.html', {'dealers': dealers})
+    return render(request, 'dealers.html', {
+        'dealers': dealers,
+        'user': request.user
+    })

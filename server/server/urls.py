7ac6157a0f4eb9d4f7e83afa2abb6f46d11ac512
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-25 01:18:31 +0530

    Update urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,18 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Home (optional)
-    path('', include('djangoapp.urls')),
-]
-from django.contrib import admin
-from django.urls import path
-from djangoapp import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.get_dealers),
-    path('dealers/', views.get_dealers),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('', include('djangoapp.urls')),  # 🔥 important
 ]

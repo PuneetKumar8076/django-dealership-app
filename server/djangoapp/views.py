bba62c261be9c52f967adbfa6678f8acfdbe5a1d
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-24 21:28:39 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,7 +10,7 @@ def contact(request):
     return render(request, 'Contact.html')
 
 
-# ✅ Q16 / Q17 MAIN FUNCTION
+#  Q16 / Q17 MAIN FUNCTION
 def get_dealers(request):
     dealers = [
         {"name": "BMW Delhi", "city": "Delhi"},

232be6596a1d9210643950d25fd69ea6780f3f1f
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-24 22:13:33 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
+    path('dealers', views.get_dealers_loggedin, name='dealers'),
 ]

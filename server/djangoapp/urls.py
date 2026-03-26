ff549938d2acd7fcf45ac64e1f9c475de793d1d1
Puneet Kumar <kumarmahesh3594l@gmail.com>
2026-03-26 21:46:43 +0530

    fix login route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.get_dealers, name='home'),
     path('dealers/', views.get_dealers_loggedin, name='dealers'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 
     path('dealers/<str:state>/', views.get_dealers_by_state, name='dealers_by_state'),
 

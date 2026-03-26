a37d7c4b9aaecd8731c6060d7775f127aaa2d81f
Puneet Kumar <kumarmahesh3594l@gmail.com>
2026-03-26 22:26:06 +0530

    fix auth urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('djangoapp.urls')),  # 🔥 important
+
+    path('accounts/', include('django.contrib.auth.urls')), 
+
+    path('', include('djangoapp.urls')),
 ]

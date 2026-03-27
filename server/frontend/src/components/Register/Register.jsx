4318d580bc1ef0cf6491224b67ac55baa5059c64
Puneet Kumar <kumarmahesh3594l@gmail.com>
2026-03-27 22:13:02 +0530

    fixed register form

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -6,24 +6,31 @@ function Register() {
     first_name: "",
     last_name: "",
     email: "",
-    password: "",
+    password: ""
   });
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Registered Successfully!");
+  };
+
   return (
     <div>
       <h2>Sign Up</h2>
 
-      <input name="username" placeholder="Username" onChange={handleChange} /><br />
-      <input name="first_name" placeholder="First Name" onChange={handleChange} /><br />
-      <input name="last_name" placeholder="Last Name" onChange={handleChange} /><br />
-      <input name="email" placeholder="Email" onChange={handleChange} /><br />
-      <input name="password" type="password" placeholder="Password" onChange={handleChange} /><br />
+      <form onSubmit={handleSubmit}>
+        <input name="username" placeholder="Username" onChange={handleChange} /><br /><br />
+        <input name="first_name" placeholder="First Name" onChange={handleChange} /><br /><br />
+        <input name="last_name" placeholder="Last Name" onChange={handleChange} /><br /><br />
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} /><br /><br />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} /><br /><br />
 
-      <button>Register</button>
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
